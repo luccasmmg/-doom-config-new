6ccd52445cfd678f6c5ca9a0c9683552bfb90596
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2022-12-17 19:46:48 -0300

    feat: added a custom image on splash

--- config.el
+++ config.el
@@ -21,6 +21,7 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "Fira Code" :size 11))
+(setq fancy-splash-image (concat doom-user-dir "NERV.png"))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
